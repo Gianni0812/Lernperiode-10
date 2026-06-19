da3d0fcc4c95ab98d4dcc2f6848cbf430b5f12f9
Gianni0812 <g.caruso.inf24@stud.bbbaden.ch>
2026-06-19 10:52:06 +0200

    Add files via upload

---
+++ Voci-Trainer/Voci-Trainer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace VociTrainer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> terms = new List<string>()
+            {
+                "unschuldig",
+                "Strafe",
+                "Moderator*In",
+                "beraten",
+                "etw. begehen"
+            };
+            List<string> definitions = new List<string>()
+            {
+                "innocent",
+                "punishment",
+                "presenter",
+                "advise",
+                "commit"
+            };
+
+            int punkte = 0;
+
+            for(int i  = 0; i < terms.Count; i++)
+            {
+                Console.WriteLine($"Was bedeutet {terms[i]}");
+                string input = Console.ReadLine().ToLower().Trim();
+
+                if(input == definitions[i])
+                {
+                    Console.WriteLine("Korrekt!");
+                    punkte++;
+                }
+                else
+                {
+                    Console.WriteLine("Leider falsch");
+                    Console.WriteLine($"Richtig ist: {definitions[i]}");
+                }
+            }
+
+            Console.WriteLine($"Du hast {punkte} von {terms.Count} korrekt gelöst");
+        }
+    }
+}
